ac7b5c2908f1a518982d18d6769ec30a47a4d032
SantiB17 <perrochato08@gmail.com>
2020-10-06 14:47:16 -0700

    adding a small CNN model

--- app.py
+++ app.py
@@ -10,9 +10,21 @@ if os.path.isdir('train/santi') is False:
     os.makedirs('valid/santi')
     os.makedirs('test/santi')
 
-for c in random.sample(glob.glob('santi*'), 234):
-    shutil.move(c, 'train/santi')
-for c in random.sample(glob.glob('santi*'), 29):
-    shutil.move(c, 'valid/santi')
-for c in random.sample(glob.glob('santi*'), 29):
-    shutil.move(c, 'test/santi')
+    for c in random.sample(glob.glob('santi*'), 234):
+        shutil.move(c, 'train/santi')
+    for c in random.sample(glob.glob('santi*'), 29):
+        shutil.move(c, 'valid/santi')
+    for c in random.sample(glob.glob('santi*'), 29):
+        shutil.move(c, 'test/santi')
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300,300,3)),
+    tf.keras.layers.MaxPool2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2)
+])
+
