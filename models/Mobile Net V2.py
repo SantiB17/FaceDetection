b717475764009a0eb03046f35f5a26a20a00e054
SantiB17 <perrochato08@gmail.com>
2020-10-21 02:58:05 -0700

    Implemented data_augmentation with the Sequential API since it yielded better results as opposed to using it with the ImageDataGenerator. Built and trained a  model on top of Mobile Net V2 with the Functional API.

--- models/Mobile Net V2.py
+++ models/Mobile Net V2.py
@@ -3,19 +3,24 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import splitfolders
+
 
 IMG_SIZE = (160,160)
 
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
 # Data preprocessing boilerplate code
 train_datagen = ImageDataGenerator(
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
+    # rotation_range=40,
+    # width_shift_range=0.2,
+    # height_shift_range=0.2,
+    # shear_range=0.2,
+    # zoom_range=0.2,
+    # horizontal_flip=True,
+    # fill_mode='nearest'
 )
 validation_datagen = ImageDataGenerator()
 test_datagen = ImageDataGenerator()
@@ -40,10 +45,33 @@ test_generator = test_datagen.flow_from_directory(
     class_mode='binary'
     )
 
-preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
-
 # Create the base model from the pre-trained model MobileNet V2
 IMG_SHAPE = IMG_SIZE + (3,)
 base_model = keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
                                             include_top=False,
                                             weights='imagenet')
+
+base_model.trainable = False
+base_model.summary()
+
+preprocess_input = keras.applications.mobilenet_v2.preprocess_input
+global_average_layer = keras.layers.GlobalAveragePooling2D()
+pred_layer = keras.layers.Dense(1)
+
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(inputs)
+x = base_model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = pred_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+model.compile(optimizer=keras.optimizers.Adam(lr=0.0001),
+              loss=keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+initial_epochs = 10
+history = model.fit(train_generator,
+                    epochs=initial_epochs,
+                    validation_data=validation_generator)
