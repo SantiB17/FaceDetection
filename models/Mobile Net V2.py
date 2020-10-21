51bf026a6d2fdd06d3576d51134fe4a9719bae1b
SantiB17 <perrochato08@gmail.com>
2020-10-21 02:09:02 -0700

    Created the base model with Mobile Net V2 using the imagenet weights. Also removed rescale argument from ImageDataGenerator since mobilenet_v2.preprocessing can rescale the pixels.

--- models/Mobile Net V2.py
+++ models/Mobile Net V2.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import splitfolders
 
+IMG_SIZE = (160,160)
+
 # Data preprocessing boilerplate code
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
     rotation_range=40,
     width_shift_range=0.2,
     height_shift_range=0.2,
@@ -17,24 +17,33 @@ train_datagen = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode='nearest'
 )
-validation_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255.)
+validation_datagen = ImageDataGenerator()
+test_datagen = ImageDataGenerator()
 
 train_generator = train_datagen.flow_from_directory(
-    'data/train',
-    target_size=(300, 300),
+    'C:/Users/perro/PycharmProjects/cv_proj/data/train',
+    target_size=IMG_SIZE,
     batch_size=30,
     class_mode='binary'
 )
+
 validation_generator = validation_datagen.flow_from_directory(
-    'data/val',
-    target_size=(300, 300),
+    'C:/Users/perro/PycharmProjects/cv_proj/data/val',
+    target_size=IMG_SIZE,
     batch_size=10,
     class_mode='binary'
 )
 test_generator = test_datagen.flow_from_directory(
-    'data/test',
-    target_size=(300,300),
+    'C:/Users/perro/PycharmProjects/cv_proj/data/test',
+    target_size=IMG_SIZE,
     batch_size=5,
     class_mode='binary'
     )
+
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+# Create the base model from the pre-trained model MobileNet V2
+IMG_SHAPE = IMG_SIZE + (3,)
+base_model = keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                            include_top=False,
+                                            weights='imagenet')
