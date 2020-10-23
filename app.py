258b34d3a6f8de892c78c27f17fc6386795bfc08
SantiB17 <perrochato08@gmail.com>
2020-10-23 11:57:43 -0700

    Testing out function to split data into appropriate folders in app.py, works as expected.

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from util.functions import predict_image
+from util.functions import predict_image, split_data
 
 path = 'playing_images/santi.jpg'
 predict_image(path)
+
+source_path = 'C:/Users/perro/OneDrive/Desktop/test_not_santi'
+train_path = 'file_test/train_temp/Not Santi'
+val_path = 'file_test/val_temp/Not Santi'
+test_path = 'file_test/test_temp/Not Santi'
+
+split_data(source_path, train_path, val_path, test_path, 0.9)
