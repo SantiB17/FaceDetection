3ae9c829a6b0fb66beab236af4745801abe72d32
SantiB17 <perrochato08@gmail.com>
2020-10-23 10:04:14 -0700

    app.py is now a driver script. Also added a copy_to helper function to copy files to other directories.

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 from util.functions import predict_image
 
+path = 'playing_images/santi.jpg'
+predict_image(path)
--- util/functions.py
+++ util/functions.py
@@ -1,10 +1,12 @@
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
+import os
+import shutil
 import numpy as np
 
 
 def predict_image(path):
-    model = keras.models.load_model("models/my_keras_model.h5")
+    model = keras.models.load_model("models/mobile_net_v2.h5")
     img = image.load_img(path, target_size=(300, 300))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -15,3 +17,11 @@ def predict_image(path):
         print('Santi')
     else:
         print('Not Santi')
+
+def copy_to(DIR, LIST, SOURCE):
+    for file in LIST:
+        if os.path.getsize(os.path.join(SOURCE, file)) == 0:
+            continue
+        source_path = os.path.join(SOURCE, file)
+        dest_path = os.path.join(DIR, file)
+        shutil.copyfile(source_path, dest_path)
